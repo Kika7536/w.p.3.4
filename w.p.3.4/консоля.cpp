f16f5abc1565e33de21f341ab6adb04e6e73b009
Kika7536 <kika7536.p@gmail.com>
2024-09-08 19:57:41 +0300

    Добавьте файлы проекта.

---
+++ w.p.3.4/консоля.cpp
@@ -0,0 +1,29 @@
+﻿#include<iostream>
+using namespace std;
+
+//При учете, что отрицательный опыт тоже опыт все корректно
+
+void main() {
+	setlocale(LC_ALL, "rus");
+
+	int sk;
+
+	cout << "Введите число очков опыта: ";
+	cin >> sk;
+	cout << "-----Считаем-----" << endl;
+
+	if (sk < 1000) {
+		cout << "Ваш уровень: 1"  << endl;
+	}
+	else if (sk < 2500) {
+		cout << "Ваш уровень: 2" << endl;
+	}
+	else if (sk < 5000) {
+		cout << "Ваш уровень: 3" << endl;
+	}
+	else {
+		cout << "Ваш уровень: 4" << endl;
+
+	}
+
+}
